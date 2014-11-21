ef039aa74790de9f47188ba90fd74e05c4a31cfe
Artemy <orthograf@bk.ru>
2014-11-21 10:50:49 +0600

    update config

--- settings/config/config.php
+++ settings/config/config.php
@@ -59,7 +59,7 @@ $config['view']['show_rating'] = false;
  */
 
 //$config['view']['header']['logo']['file'] = 'logo.png';
-$config['view']['header']['logo']['url'] = 'common/templates/skin/portal/assets/images/a.png';
+$config['view']['header']['logo']['url'] = '/common/templates/skin/portal/assets/images/a.png';
 //$config['view']['header']['logo']['name'] = Config::Get('view.name');
 $config['view']['header']['logo']['name'] = 'Template <span class="logo-name-color"> for AltoCMS</span>';
 
